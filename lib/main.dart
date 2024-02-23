db52552c74e65861f576379d15085fb4b8bbf43e
Erbosha01 <aibasheverbol@gmail.com>
2024-02-23 23:43:41 +0600

    add AutoRoute

--- lib/main.dart
+++ lib/main.dart
@@ -1,258 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:rhymer/router/router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RhymerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RhymerApp extends StatefulWidget {
+  const RhymerApp({super.key});
+
+  @override
+  State<RhymerApp> createState() => _RhymerAppState();
+}
+
+class _RhymerAppState extends State<RhymerApp> {
+  final _router = AppRouter();
 
   @override
   Widget build(BuildContext context) {
     const primaryColor = Color(0xFFF82B10);
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp.router(
+      title: 'Rhymer',
       theme: ThemeData(
         primaryColor: primaryColor,
         scaffoldBackgroundColor: const Color(0xFFEFF1F3),
         colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({
-    super.key,
-  });
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  var _selectedPageIndex = 0;
-  final _pageController = PageController();
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return Scaffold(
-      body: PageView(
-          controller: _pageController,
-          onPageChanged: (value) {
-            setState(() => _selectedPageIndex = value);
-          },
-          children: const [
-            SearchScreen(),
-            Scaffold(),
-            Scaffold(),
-            Scaffold(),
-          ]),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: theme.primaryColor,
-        unselectedItemColor: theme.hintColor,
-        currentIndex: _selectedPageIndex,
-        onTap: _openPage,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Поиск"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite), label: "Избранное"),
-          BottomNavigationBarItem(icon: Icon(Icons.menu), label: "Стрихи"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings), label: "Настройки"),
-        ],
-      ),
-    );
-  }
-
-  void _openPage(int index) {
-    setState(() => _selectedPageIndex = index);
-    _pageController.animateToPage(index,
-        duration: const Duration(milliseconds: 300), curve: Curves.linear);
-  }
-}
-
-class SearchScreen extends StatelessWidget {
-  const SearchScreen({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        const SliverAppBar(
-          pinned: true,
-          snap: true,
-          floating: true,
-          title: Text("Rhymer"),
-          elevation: 0,
-          surfaceTintColor: Colors.transparent,
-          bottom: PreferredSize(
-            preferredSize: Size.fromHeight(70),
-            child: SearchButton(),
-          ),
-        ),
-        const SliverToBoxAdapter(
-          child: SizedBox(
-            height: 16,
-          ),
-        ),
-        SliverToBoxAdapter(
-          child: SizedBox(
-            height: 100,
-            child: ListView.separated(
-              separatorBuilder: (context, index) => const SizedBox(
-                width: 16,
-              ),
-              padding: const EdgeInsets.only(left: 16),
-              scrollDirection: Axis.horizontal,
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                final rhymes = List.generate(4, (index) => "Рифма $index");
-                return RhymeHistoryCard(rhymes: rhymes);
-              },
-            ),
-          ),
-        ),
-        const SliverToBoxAdapter(
-          child: SizedBox(
-            height: 16,
-          ),
-        ),
-        SliverList.builder(
-            itemBuilder: (context, index) => const RhymeListCard()),
-      ],
-    );
-  }
-}
-
-class RhymeHistoryCard extends StatelessWidget {
-  const RhymeHistoryCard({
-    super.key,
-    required this.rhymes,
-  });
-
-  final List<String> rhymes;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return BaseContainer(
-      padding: const EdgeInsets.all(16),
-      width: 200,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Слово",
-            style: theme.textTheme.bodyLarge
-                ?.copyWith(fontWeight: FontWeight.w700),
-          ),
-          Wrap(
-            children: rhymes
-                .map((e) => Padding(
-                      padding: const EdgeInsets.only(right: 4),
-                      child: Text(e),
-                    ))
-                .toList(),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class BaseContainer extends StatelessWidget {
-  const BaseContainer(
-      {super.key,
-      required this.child,
-      required this.width,
-      this.margin,
-      this.padding = const EdgeInsets.only(left: 12)});
-
-  final double width;
-  final Widget child;
-  final EdgeInsets? margin;
-  final EdgeInsets padding;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return Container(
-      width: width,
-      margin: margin,
-      padding: padding,
-      decoration: BoxDecoration(
-          color: theme.cardColor, borderRadius: BorderRadius.circular(12)),
-      child: child,
-    );
-  }
-}
-
-class RhymeListCard extends StatelessWidget {
-  const RhymeListCard({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return BaseContainer(
-      margin: const EdgeInsets.symmetric(horizontal: 16).copyWith(bottom: 10),
-      width: double.infinity,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            "Рифма",
-            style: theme.textTheme.bodyLarge,
-          ),
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.favorite,
-                color: theme.hintColor.withOpacity(0.2),
-              ))
-        ],
-      ),
-    );
-  }
-}
-
-class SearchButton extends StatelessWidget {
-  const SearchButton({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 16).copyWith(bottom: 8),
-      padding: const EdgeInsets.all(12),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: theme.hintColor.withOpacity(0.1),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Row(
-        children: [
-          const Icon(Icons.search_rounded),
-          const SizedBox(width: 12),
-          Text(
-            "Поиск рифм...",
-            style: TextStyle(
-                fontSize: 18,
-                color: theme.hintColor.withOpacity(0.4),
-                fontWeight: FontWeight.w500),
-          ),
-        ],
-      ),
+      routerConfig: _router.config(),
     );
   }
 }
---
+++ lib/router/router.dart
@@ -0,0 +1,21 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:rhymer/features/favorites/favorites.dart';
+import 'package:rhymer/features/home/home.dart';
+import 'package:rhymer/features/poems/poems.dart';
+import 'package:rhymer/features/search/search.dart';
+import 'package:rhymer/features/settings/settings.dart';
+
+part 'router.gr.dart';
+
+@AutoRouterConfig()
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(page: HomeRoute.page, path: "/", children: [
+          AutoRoute(page: FavoritesRoute.page, path: "favorites"),
+          AutoRoute(page: SearchRoute.page, path: "search"),
+          AutoRoute(page: SettingsRoute.page, path: "settings"),
+          AutoRoute(page: PoemsRoute.page, path: "poems"),
+        ]),
+      ];
+}
